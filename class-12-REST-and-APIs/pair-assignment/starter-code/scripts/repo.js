f364d2865d33b347167331b4f7100cfbe619fa53
Amy Leek <amyeleek@gmail.com>
2016-01-19 13:17:09 -0800

    All TODOs completed, Github API used and returning properly

--- class-12-REST-and-APIs/pair-assignment/starter-code/scripts/repo.js
+++ class-12-REST-and-APIs/pair-assignment/starter-code/scripts/repo.js
@@ -4,16 +4,31 @@
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    // TODO: How would you like to fetch your repos? Don't forget to call the callback.
+    // DONE: How would you like to fetch your repos? Don't forget to call the callback.
+    var qs = '?per_page=100&sort=updated';
+    // $.get('/github/user/MarsDragon/repos' + qr).done(function(data, msg, xhr){
+    //   repos.all = data;
+    // }).done(callback);
 
+    $.ajax({
+      url: 'https://api.github.com/users/MarsDragon/repos' + qs,
+      type: 'GET',
+      headers: {'Authorization': 'token ' + github.getToken()},
+      success: function(data, msg, xhr){
+        repos.all = data;
+      }
+    }).done(callback);
   };
 
   // DONE: Model method that filters the full collection for repos with a particular attribute.
   // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
   repos.with = function(attr) {
-    return repos.all.filter(function(repo) {
+    console.log('repos.with called');
+    var data = repos.all.filter(function(repo) {
       return repo[attr];
     });
+
+    return data;
   };
 
   module.repos = repos;
